3942f3ef9b68b5e537e7663469a3a5199953eaf3
Miguel Barcelo <miguel.barcelo@udr.com>
2025-03-22 17:26:34 -0700

    chore: place Order status consts in shared folder

---
+++ src/shared/constants/status.js
@@ -0,0 +1,9 @@
+module.exports = {
+  Order: {
+    pending: "pending",
+    inventoryConfirmed: "inventory_confirmed",
+    outOfStock: "out_of_stock",
+    paymentConfirmed: "payment_confirmed",
+    paymentFailed: "payment_failed",
+  },
+};
